c95ae0bbb42a58144bfb35b1507b0e4505c3f899
Beta (Ben) Anggoro <ben@benworkandplay.com>
2018-09-10 12:26:43 +0700

    Handlebars

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 import express from 'express';
+import exphbs from 'express-handlebars';
 
 const app = express();
-app.get('/', (req, res) => res.send('Who\'s Following Who?'))
+let hbs;
+
+hbs = exphbs.create({
+    helpers: {
+        foo: () => 'FOO!',
+        bar: () => 'BAR!',
+    },
+    defaultLayout: 'main',
+    extname: '.hbs',
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.get('/', (req, res) => res.render('index'));
 
 app.listen(8000, () => console.log('Example app listening on port 8000!'))
